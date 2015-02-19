5e95f84533c4e57bdaf4be311288ab1b2e5a5d8a
mmcordova <cordova.mm@gmail.com>
2015-02-19 11:12:43 -0200

    Função division

--- GitHub1/Source.cpp
+++ GitHub1/Source.cpp
@@ -7,11 +7,18 @@ double sum(double a, double b)
 	return a + b;
 }
 
+double division(double a, double b)
+{
+	return a / b;
+}
+
 int main()
 {
 	cout << "Eita" << endl;
 
-	cout << "2 + 4 = " << sum(2, 4) << endl << "Putz!";
+	cout << "2 + 4 = " << sum(2, 4) << endl << "Putz!" << endl;
+
+	cout << "2 / 6 = " << division(2, 6) << endl << "O loco bicho" << endl;
 
 	return 0;
 }
