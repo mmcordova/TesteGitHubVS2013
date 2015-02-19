d02fb1a5620551ac9fb983810e8904c9095b54e6
mmcordova <cordova.mm@gmail.com>
2015-02-19 11:09:45 -0200

    Adicionada função sum(double, double)

--- GitHub1/Source.cpp
+++ GitHub1/Source.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
+double sum(double a, double b)
+{
+	return a + b;
+}
+
 int main()
 {
 	cout << "Eita" << endl;
+
+	cout << "2 + 4 = " << sum(2, 4) << endl << "Putz!";
 
 	return 0;
 }
